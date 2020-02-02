78b040c2bb7541ea4caef078f59ddce9983c165a
jiteshkumarr <jitesh101.kumar@gmail.com>
2020-02-03 03:43:29 +0530

    First Release

---
+++ SubdomainFinder.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import dns.resolver
+
+#################
+## PREPARATION ##
+################# 
+
+domain = input("Input the Domain Name >>> ")
+list_path = "Subdomain.txt"
+
+
+#open wordlist
+try:
+	list_file = open(list_path)
+	lines = list_file.read().splitlines()
+except:
+	print("File {} not found".format(list_path))
+	sys.exit(1)
+
+#################
+## BRUTE FORCE ##
+#################
+
+for line in lines:
+	subdomain = line + "." + domain
+	try:
+		answers = dns.resolver.query(subdomain, 'a')
+		for result in answers:
+			print ("[+] {} {}".format(subdomain, result))
+	except:
+		pass
